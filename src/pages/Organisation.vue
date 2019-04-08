aada78b0c7761f6729e160600e4fc9537746da1d
Alexey Mytsykov <alexmyt1@yandex.ru>
2019-04-08 19:42:40 +0400

    Working at Organisation detail page.

--- src/pages/Organisation.vue
+++ src/pages/Organisation.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="container mb-5">
     <b-breadcrumb :items="breadcrumbs" />
-    <pageHeader :headerText="organisation.name" :headerSub="organisation.categories[0].name" />
+    <pageHeader :headerText="organisation.name" :headerSub="category.name" />
 
     <div class="row">
       <div class="col-12 col-sm-6">
         <dl class="row"> 
           <dt class="col-sm-3">Адрес</dt>
-          <dd class="col-sm-9" itemprop="address">{{organisation.addresses[0].address}}</dd>
+          <dd class="col-sm-9" itemprop="address">{{address.address}}</dd>
 
           <dt class="col-sm-3">Телефоны</dt>
           <dd class="col-sm-9 d-flex flex-column">
             <phoneNumber class="w-100" v-for="contact in phones" :key="contact.id" :contact="contact"/>
           </dd>
           
+          <template v-if="emails.length">
+          <dt class="col-sm-3">E-Mail</dt>
+          <dd v-for="contact in emails" class="col-sm-9 d-flex flex-column">
+            <span><a :href="'mailto:'+contact.contact" itemprop="email">{{contact.contact}}</a></span>
+          </dd>
+          </template>
+
+          <template v-if="urls.length">
+          <dt class="col-sm-3">WWW</dt>
+          <dd v-for="contact in urls" class="col-sm-9 d-flex flex-column">
+            <span><a :href="contact.contact"  itemprop="url">{{contact.contact}}</a></span>
+          </dd>
+          </template>
 
         </dl>
 
@@ -35,6 +48,11 @@ export default {
   data () {
     return {
       organisation: [],
+      category: {},
+      address: {},
+      phones: [],
+      emails: [],
+      urls: [],
       breadcrumbs: [
         { text: 'Главная', to: {name: 'home' }},
         { text: 'Организации', to: {name: 'organisations'}},
@@ -47,16 +65,23 @@ export default {
     .then(response => {
       this.organisation = response.data;
       this.breadcrumbs.push({text: response.data.name, href: '#' });
-    })
-  },
-
-  computed:{
-    phones: function() {
-      return this.organisation.contacts.filter( function(contact){
-        return contact.contact_type == 'phone';
+      this.category = response.data.categories[0];
+      this.address = response.data.addresses[0];
+      
+      this.organisation.contacts.forEach(element => {
+        switch(element.contact_type){
+          case 'phone': 
+            this.phones.push(element);
+            break;
+          case 'email':
+            this.emails.push(element);
+            break;
+          case 'uri':
+            this.urls.push(element);
+            break;
+        };
       });
-    },
-
-  }
+    });
+  },
 }
 </script>
